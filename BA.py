5afa575d444f6afd4e774124782813368ce8cde7
Yichen Liu <liuyichenpanda@gmail.com>
2017-03-16 18:54:55 +0000

    all work

--- BA.py
+++ BA.py
@@ -1,7 +1,29 @@
 import numpy as np
 import networkx as nx
-import matplotlib.pyplot as plt
 import random
+
+
+def PRA(N,m):
+    A = nx.Graph()
+    A.add_edges_from([(1, 2), (2, 3), (3, 4), (4, 5), (5, 1)])
+    list_of_vertex = [1, 2, 3, 4, 5]
+
+    for i in range(6,N):
+        A.add_node(i)
+        edge_list = []
+        random_list = []
+        ncounter = 0
+        while ncounter < m:
+            ran = random.choice(list_of_vertex)
+            if ran not in random_list:
+                random_list.append(ran)
+                edge_list.append((i,ran))
+                ncounter += 1
+
+        A.add_edges_from(edge_list)
+        list_of_vertex.append(i)
+
+    return A
 
 
 def BA(N, m):  # N number of vertex=time, m number of degree on each vertex
@@ -26,83 +48,18 @@ def BA(N, m):  # N number of vertex=time, m number of degree on each vertex
                 ncounter += 1
 
         if len(edge_list) != m:
-            raise Exception("too less edges created")
+            raise Exception("too few edges created")
         A.add_edges_from(edge_list)
         for e in edge_list:
             list_of_vertex.append(e[0])
             list_of_vertex.append(e[1])
 
     return A
-
-
-def choose(list_of_nodes, list_of_ran):
-    ran = random.choice(list_of_nodes)
-    if ran not in list_of_ran:
-        return ran
-    else:
-        choose(list_of_nodes, list_of_ran)
-
-def generate_BA(N, m):  # N number of vertex=time, m number of degree on each vertex
-    # setup initial graph
-    A = nx.Graph()
-    # time evolve
-    A.add_edges_from([(1, 2), (2, 3),(3,4),(4,5),(5,1)])
-
-    list_of_vertex = [1,2,2,3,3,4,4,5,5,1]
-
-    # add new node
-    for i in range(6, N):
-        A.add_node(i)
-        edge_list = []
-        random_list = []
-        for n in range(m):
-            ran = choose(list_of_vertex, random_list)
-            random_list.append(ran)
-            edge_list.append((i,ran))
-        if len(edge_list) != m:
-            raise Exception("too less edges created")
-        A.add_edges_from(edge_list)
-        for e in edge_list:
-            list_of_vertex.append(e[0])
-            list_of_vertex.append(e[1])
-
-    return A
-
-
-def generate_BA_Multi(N, m):  # N number of vertex=time, m number of degree on each vertex
-    # setup initial graph
-    G = nx.MultiGraph()
-    # time evolve
-    G.add_edges_from([(1, 2), (2, 3),(3,4),(4,5),(5,1)])
-
-    list_of_vertex = [1,2,2,3,3,4,4,5,5,1]
-
-    # Add linked existing vertex to list
-    # for i in G.edges():
-    #     list_of_vertex.append(i[0])
-    #     list_of_vertex.append(i[1])
-
-    # add new node
-    for i in range(6, N + 1):
-        G.add_node(i)
-        edge_list = []
-        #print edge_list
-        for n in range(m):
-            edge_list.append((i, random.choice(list_of_vertex)))
-        if len(edge_list) != m:
-            raise Exception("too less edge")
-        #print edge_list
-        G.add_edges_from(edge_list)
-        for e in edge_list:
-            list_of_vertex.append(e[0])
-            list_of_vertex.append(e[1])
-        #print list_of_vertex
-    return G
 
 
 def degree_frequency(G):
     freq_list = nx.degree_histogram(G)
-    normalise = np.array(freq_list) / float((len(freq_list)-1))
+    normalise = np.array(freq_list) / float(G.number_of_nodes())
     return np.arange(len(freq_list)), normalise
 
 
@@ -115,7 +72,4 @@ def av_k(G):
     return np.mean(arr)
 
 
-if __name__ == "__main__":
-    G = generate_BA(2000, 2)
-    # nx.draw(G)
-    # plt.show()
+
--- test.py
+++ test.py
@@ -3,12 +3,16 @@ import log_bin_CN_2016 as lb
 import matplotlib.pyplot as plt
 import numpy as np
 
-G = generate_BA(100000,2)
+G = BA(int(1e5),3)
+B = BA(int(1e5),3)
 val, freq = degree_frequency(G)
+val2, freq2 = degree_frequency(B)
 data = degree_to_logbin(G)
-c, b = lb.log_bin(data,1.,1.,1.2,drop_zeros=False)
+
+c, b = lb.log_bin(data,3.,1.,1.5,drop_zeros=True)
 print av_k(G)
 plt.loglog(val, freq, "x", label="Frequency")
+plt.loglog(val2, freq2, ".", label="Frequency")
 plt.loglog(c,b, "r-", label="Log binned")
 plt.xlabel("k")
 plt.ylabel("Frequency")
